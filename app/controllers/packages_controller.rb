557439b8a2ddc4ece8de898ff9d399a93c704c3b
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-04-29 09:36:05 +0300

    Update packages_controller.rb
    added create package for the submit button

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,4 +1,10 @@
 class PackagesController < ApplicationController
   def create
+  @package = Package.new(package_params)
+
+  if @package.save
+    render json: { message: "Package created successfully", package: @package }, status: :created
+  else
+    render json: { errors: @package.errors.full_messages }, status: :unprocessable_entity
   end
 end
