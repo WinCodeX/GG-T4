5325311f272772fbb6f1e84252db7a2c75a3460d
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-04-30 07:49:38 +0300

    Update registrations_controller.rb
    added functionality to edit profile

--- app/views/users/registrations/registrations_controller.rb
+++ app/views/users/registrations/registrations_controller.rb
@@ -1,15 +1,21 @@
 class Users::RegistrationsController < Devise::RegistrationsController
     protected
   
-    # Only ask for password if sensitive fields are changed
-    def update_resource(resource, params)
-      if params[:password].present? || params[:email].present?
-        # Require current password if email or password are being updated
-        super
-      else
-        # Allow update without password
-        resource.update_without_password(params.except(:current_password))
-      end
-    end
+    # Only ask for password if sensitive fields are 
+def update
+  self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+  if account_update_params[:password].present?
+    result = resource.update_with_password(account_update_params)
+  else
+    result = resource.update_without_password(account_update_params.except(:current_password))
   end
-  
+
+  if result
+    set_flash_message :notice, :updated
+    respond_with resource, location: after_update_path_for(resource)
+  else
+    clean_up_passwords resource
+    render :edit, status: :unprocessable_entity
+  end
+end
