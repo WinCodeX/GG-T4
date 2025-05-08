661080fd60f1dab6c428ff20fd5f45499fbb799b
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-08 19:13:00 +0300

    Update sessions_controller.rb

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,16 +1,49 @@
-class Api::V1::SessionsController < Devise::SessionsController
-    respond_to :json
+# app/controllers/api/v1/sessions_controller.rb
+module Api
+  module V1
+    class SessionsController < Devise::SessionsController
+      respond_to :json
 
-    before_action :authenticate_user!
-  
-    private
-  
-    def respond_with(resource, _opts = {})
-      render json: { token: request.env['warden-jwt_auth.token'], user: resource }
-    end
-  
-    def respond_to_on_destroy
-      head :no_content
+      # mobile clients don't include a CSRF token
+      skip_before_action :verify_authenticity_token, raise: false
+
+      # don't require you to be logged in just to POST /login
+      skip_before_action :authenticate_user!, only: :create
+
+      # -------------------------------------------------------
+      # POST /api/v1/login
+      # -------------------------------------------------------
+      def create
+        super
+      end
+
+      # -------------------------------------------------------
+      # DELETE /api/v1/logout
+      # -------------------------------------------------------
+      def destroy
+        super
+      end
+
+      private
+
+      # on successful login, Devise::JWT has already set
+      # `request.env['warden-jwt_auth.token']` for us.
+      # We just pluck it out and render JSON.
+      def respond_with(resource, _opts = {})
+        render json: {
+          token: request.env['warden-jwt_auth.token'],
+          user: {
+            id:    resource.id,
+            email: resource.email,
+            name:  resource.username || resource.name
+          }
+        }, status: :ok
+      end
+
+      # on logout we simply return 204 No Content
+      def respond_to_on_destroy
+        head :no_content
+      end
     end
   end
-  
+end
