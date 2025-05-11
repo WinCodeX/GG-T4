c34b5c20aefb877f980e0b4519bba49012f484f8
WinCodeX <glenwinterg970@gmail.com>
2025-05-11 10:01:44 +0300

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ protected
 
 def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :avatar])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
   end
 
