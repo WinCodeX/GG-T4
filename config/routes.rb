8669a22c21167828fd75e357a8f59d9f45c1a22c
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-11 10:20:03 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'packages/create'
   devise_for :users, controllers: {
-  registrations: "accounts/registrations"
+  registrations: "users/registrations"
 }
 
 
