b96af1bcd631356e1b47cbf6b7464503a3b81041
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-11 09:56:57 +0300

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -36,6 +36,28 @@ class Users::RegistrationsController < Devise::RegistrationsController
     redirect_to edit_user_registration_path, notice: "Avatar removed."
   end
   
+
+def update
+  self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+  # If you're not changing password, allow update without current password
+  if account_update_params[:password].present?
+    success = resource.update_with_password(account_update_params)
+  else
+    success = resource.update_without_password(account_update_params.except(:current_password))
+  end
+
+  if success
+    set_flash_message :notice, :updated if is_flashing_format?
+    bypass_sign_in(resource)
+    redirect_to after_update_path_for(resource)
+  else
+    clean_up_passwords resource
+    respond_with resource, status: :unprocessable_entity
+  end
+end
+
+
   def create
     build_resource(sign_up_params)
 
