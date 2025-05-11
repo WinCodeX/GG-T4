950b2a5aee7476af28897f1a395e76e496eac01f
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-11 10:11:41 +0300

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,63 +1,31 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_permitted_parameters, only: [:create, :update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  # PATCH/PUT /resource
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+    # If the user provides a new password, use it
+    if account_update_params[:password].present?
+      success = resource.update_with_password(account_update_params)
+    else
+      # Otherwise, update without requiring current password
+      success = resource.update_without_password(account_update_params.except(:current_password))
+    end
+
+    if success
+      set_flash_message :notice, :updated if is_flashing_format?
+      bypass_sign_in(resource)
+      redirect_to after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource, status: :unprocessable_entity
+    end
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # app/controllers/users/registrations_controller.rb
-  before_action :configure_permitted_parameters, only: [:create, :update]
-  def remove_avatar
-    current_user.avatar.purge_later
-    redirect_to edit_user_registration_path, notice: "Avatar removed."
-  end
-  
-
-def update
-  self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-
-  # If you're not changing password, allow update without current password
-  if account_update_params[:password].present?
-    success = resource.update_with_password(account_update_params)
-  else
-    success = resource.update_without_password(account_update_params.except(:current_password))
-  end
-
-  if success
-    set_flash_message :notice, :updated if is_flashing_format?
-    bypass_sign_in(resource)
-    redirect_to after_update_path_for(resource)
-  else
-    clean_up_passwords resource
-    respond_with resource, status: :unprocessable_entity
-  end
-end
-
-
   def create
     build_resource(sign_up_params)
 
@@ -73,12 +41,17 @@ end
         redirect_to after_inactive_sign_up_path_for(resource)
       end
     else
-      # DEBUG: Show validation errors in console
       Rails.logger.debug resource.errors.full_messages
       clean_up_passwords resource
       set_minimum_password_length
       respond_with resource, status: :unprocessable_entity
     end
+  end
+
+  # Custom action to remove avatar
+  def remove_avatar
+    current_user.avatar.purge_later
+    redirect_to edit_user_registration_path, notice: "Avatar removed."
   end
 
   protected
@@ -88,36 +61,3 @@ end
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar])
   end
 end
-
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
