0992a9459e23f280ccde8982bdd426bbccd15d69
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-14 09:06:05 +0300

    Update chat_rooms_controller.rb

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -10,7 +10,9 @@ class ChatRoomsController < ApplicationController
     end
 
     def index
-  @chat_rooms = ChatRoom.for_user(current_user)
+  @chat_rooms = ChatRoom.where(
+    "sender_id = ? OR receiver_id = ?", current_user.id, current_user.id
+  )
 
   if turbo_frame_request?
     render partial: "chat_rooms/index", locals: { chat_rooms: @chat_rooms }
