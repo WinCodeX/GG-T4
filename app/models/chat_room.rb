96e94ef34053ca52d1626b11b2c40a28ff0071e5
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-14 11:57:12 +0300

    Update chat_room.rb

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,20 +1,22 @@
-# app/models/chat_room.rb
 class ChatRoom < ApplicationRecord
-    belongs_to :sender, class_name: 'User'
-    belongs_to :receiver, class_name: 'User'
-    has_many :messages, dependent: :destroy
-  
-    validates :sender_id, uniqueness: { scope: :receiver_id }
-  
-    def self.between(user1, user2)
-      where(sender: user1, receiver: user2)
-        .or(where(sender: user2, receiver: user1))
-        .first
-    end
-    
-    def other_participant(user)
-      user == sender ? receiver : sender
-    end
+  belongs_to :sender, class_name: 'User'
+  belongs_to :receiver, class_name: 'User'
+  has_many :messages, dependent: :destroy
 
+  validates :sender_id, uniqueness: { scope: :receiver_id }
+
+  scope :for_user, ->(user) {
+    where("sender_id = :id OR receiver_id = :id", id: user.id)
+  }
+
+  def self.between(user1, user2)
+    where(sender_id: user1.id, receiver_id: user2.id)
+      .or(where(sender_id: user2.id, receiver_id: user1.id))
+      .first
   end
-  
+
+  def other_participant(user)
+    raise ArgumentError, "User not part of this chat" unless [sender_id, receiver_id].include?(user.id)
+    user == sender ? receiver : sender
+  end
+end
