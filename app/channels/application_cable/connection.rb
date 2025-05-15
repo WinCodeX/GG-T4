6962c68c7518af80d9f456a6c2e1a64837ca4eef
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-15 08:57:22 +0300

    Update connection.rb
    online now functionality

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+      OnlineStatusTracker.mark_online(current_user.id)
+    end
+
+    def disconnect
+      OnlineStatusTracker.mark_offline(current_user.id)
+    end
+
+    protected
+
+    def find_verified_user
+      env['warden'].user || reject_unauthorized_connection
+    end
   end
 end
