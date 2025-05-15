767389d256f678433b1d36d74182832c74d759e3
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-15 09:13:16 +0300

    Update redis.rb
    newredis name

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,9 @@
 # config/initializers/redis.rb
+
 require 'redis'
 require 'connection_pool'
 
-RedisClient = ConnectionPool.new(size: 5, timeout: 5) do
-  Redis.new(url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" })
+# Use a custom name to avoid conflicts with redis-client gem
+AppRedis = ConnectionPool.new(size: 5, timeout: 5) do
+  Redis.new(url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1"))
 end
