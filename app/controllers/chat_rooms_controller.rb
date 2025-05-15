2274bc5da161e414fa9cde92b70002893b6f6914
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-15 07:27:26 +0300

    Update chat_rooms_controller.rb

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,13 +1,27 @@
 class ChatRoomsController < ApplicationController
     before_action :authenticate_user!
   layout "application"
+
     def chat_with
-      other_user = User.find(params[:id])
-      @chat_room = ChatRoom.between(current_user, other_user) ||
-                   ChatRoom.create(sender: current_user, receiver: other_user)
-  
-      redirect_to chat_room_path(@chat_room)
+  other_user = User.find(params[:id])
+  @chat_room = ChatRoom.between(current_user, other_user) ||
+               ChatRoom.create(sender: current_user, receiver: other_user)
+
+  @messages = @chat_room.messages.includes(:user)
+  @chat_with = other_user
+  @chat_rooms = ChatRoom.for_user(current_user)
+
+  respond_to do |format|
+    format.turbo_stream do
+      render turbo_stream: turbo_stream.replace(
+        "dashboard",
+        partial: "chat_rooms/show",
+        locals: { chat_room: @chat_room, messages: @messages, chat_with: @chat_with, chat_rooms: @chat_rooms }
+      )
     end
+    format.html { redirect_to chat_room_path(@chat_room) } # fallback
+  end
+end
 
     def index
   @chat_rooms = ChatRoom.for_user(current_user)
