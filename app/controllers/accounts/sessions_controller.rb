8f36ab084c851454d1bae5d3c26ea59068f5138d
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-15 10:24:42 +0300

    Update sessions_controller.rb

--- app/controllers/accounts/sessions_controller.rb
+++ app/controllers/accounts/sessions_controller.rb
@@ -1,12 +1,2 @@
-class Accounts::SessionsController < Devise::SessionsController
-  protected
-
-  def respond_with(resource, _opts = {})
-    respond_to do |format|
-      format.html { super }
-      format.turbo_stream {
-        render turbo_stream: turbo_stream.replace("flash", partial: "shared/flash", locals: { flash: flash })
-      }
-    end
-  end
+class Accounts::SessionsController < 
 end
