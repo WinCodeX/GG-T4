e602062346cab3fe083629970b9c71446253c085
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-16 00:04:37 +0300

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import SubmitOnEnterController from "./controllers/submit_on_enter_controller"
+application.register("submit-on-enter", SubmitOnEnterController)
