10f3aeaa877d7fda27f0fcbae63694d00ac57d86
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-15 19:09:35 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
   registrations: "accounts/registrations",
   sessions: "accounts/sessions"
-}, [:destroy]
+}
 
 
 devise_scope :user do
