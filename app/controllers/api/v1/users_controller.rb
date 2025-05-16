19d80ea152af3825537fc65a363b0306025fe6d7
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-16 17:52:07 +0300

    Update users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UsersController < ApplicationController
     def me
       render json: {
         id: current_user.id,
-        name: current_user.name,
+        name: current_user.username,
         email: current_user.email,
         avatar: current_user.avatar.attached? ? url_for(current_user.avatar) : nil
       }
