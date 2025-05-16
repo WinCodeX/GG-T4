c7a18eda7f6b1cd26f57ff576b7c55e37feb8b7e
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-16 21:05:04 +0300

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*' # Change this to your Expo LAN IP if needed
-  
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
+  allow do
+    origins '*'
+
+    resource '*',
+      headers: ['Authorization', 'Content-Type', 'Accept'],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Authorization']
   end
-  
+end
