369c9f15c8be4fdff594ecc40097c1ef0793b277
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-16 13:37:20 +0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -19,6 +19,8 @@ bundle exec rails db:migrate:up VERSION=20250425192850  # agents
 bundle exec rails db:migrate:up VERSION=20250425201824   # courier_services
 bundle exec rails db:migrate:up VERSION=20250425192853   # packages
 
+bundle exec rails db:migrate:up VERSION=20250511090435   # ChatRooms first
+bundle exec rails db:migrate:up VERSION=20250511090531   # Messages next
 
 bundle exec rails db:migrate:up VERSION=20250504190703
 
